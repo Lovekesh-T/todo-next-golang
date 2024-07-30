35319586827a422e849db94fd4625a2643fb8422
king100xp <damnbooyee@gmail.com>
2024-07-30 23:46:24 +0530

    added new allowed origin

--- server/main.go
+++ server/main.go
@@ -18,7 +18,7 @@ app := chi.NewRouter();
 app.Use(middleware.Logger);
 app.Use(cors.Handler(cors.Options{
     // AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
-    AllowedOrigins:   []string{"http://localhost:3000"},
+    AllowedOrigins:   []string{"http://localhost:3000","https://todo-next-golang.vercel.app"},
     // AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
     AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"},
     AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
